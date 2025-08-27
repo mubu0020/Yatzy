b41f71208808df2f7a6b775ab683c5c741fa28fc
Thea <thgy001@student.kristiania.no>
2025-08-27 13:55:59 +0200

    test

--- yatzy.test.js
+++ yatzy.test.js
@@ -1,6 +1,10 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { yatzeeScore } from "./yatzy.js";
 
 test("scoring Chance add all dice", () => {
   expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1 + 2 + 3 + 4 + 5);
+});
+
+test("How many ones did we get", () => {
+  expect(countOnes([1, 1, 3, 4, 5])).toBe(2);
 });
